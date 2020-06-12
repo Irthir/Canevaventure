7ea7b9fb438c36ce2c18a9373e79aca76cbf652e
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-06-12 14:04:35 +0200

    Ajout du système de dessin de points.

---
+++ Canevaventure/Assets/Scripts/ZoneDessin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneDessin : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Pinceau = null;
+    [SerializeField]
+    private float TaillePinceau = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            var Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(Ray, out hit))
+            {
+                var go = Instantiate(Pinceau, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+                go.transform.localScale = Vector3.one * TaillePinceau;
+            }
+        }
+    }
+}
